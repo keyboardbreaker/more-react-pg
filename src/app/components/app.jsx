4bea610ce2a8ade9d5e814af89f82a6ca6e99809
keyboardbreaker <yujin_1991@hotmail.com>
2025-04-16 13:51:57 +0100

    project setup with bootstrap

---
+++ src/app/components/app.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+
+    return (
+        <div>
+            Hello JSX!
+        </div>
+    );
+};
+
+export default App;
--- src/app/layout.js
+++ src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
